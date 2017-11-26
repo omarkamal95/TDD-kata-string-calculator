61abdc98588b9f9b32ac116e0df91de997dc9630
omarkamal95 <omarkamal95@gmail.com>
2017-11-26 14:03:08 +0200

    Initial App structure

---
+++ app.js
@@ -0,0 +1,56 @@
+
+
+
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const routes = require('./routes/index');
+
+const app = express();
+
+const env = process.env.NODE_ENV || 'development';
+app.locals.ENV = env;
+app.locals.ENV_DEVELOPMENT = env == 'development';
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use('/', routes);
+
+/// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500);
+        res.send('Error');
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send("Error");
+});
+
+
+app.set("port", process.env.PORT || 3000);
+
+var server = app.listen(app.get("port"), function() {
+  console.log("Express server listening on port " + server.address().port);
+});
+
+module.exports = app;
