6989c0224e0857bc955822edc15486aae18a5d95
omarkamal95 <omarkamal95@gmail.com>
2017-11-26 21:45:28 +0200

    Added disallowing negative numbers and throwing error with the list of numbers

--- services/string-calculator.js
+++ services/string-calculator.js
@@ -5,15 +5,24 @@ stringCalculator.addDelimitedString = function (string) {
   }
   let numArray = string.replace(/\n/g, ",").split(",");
 
-  let result =  numArray.reduce((sum, numString) => {
-    return parseInt(sum) + parseInt(numString);
+  let negativeNumbers = [];
+  let result = numArray.reduce((sum, numString) => {
+    let num = parseInt(numString);
+    if (num < 0) {
+      negativeNumbers.push(num);
+    }
+    return parseInt(sum) + num;
   }, 0);
 
-  if(typeof result === 'number' && !isNaN(result)){
-    return result
-  } else{
-    return new Error('Wrong string format')
+  if (negativeNumbers.length > 0) {
+    return new Error('Negative numbers are not allowed, the string contains: ' + negativeNumbers.toString())
   }
+
+  if (typeof result === 'number' && !isNaN(result)) {
+    return result
+  }
+
+  return new Error('Wrong string format')
 }
 
 module.exports = stringCalculator
--- tests/unittests/string-calculator-test.js
+++ tests/unittests/string-calculator-test.js
@@ -2,26 +2,34 @@ var chai = require("chai");
 var expect = chai.expect;
 var stringCalculator = require("../../services/string-calculator")
 
-describe("String Calculator", function() {
-  describe("Delimited String Addition", function() {
-    it("should return 0 when string is empty", function() {
+describe("String Calculator", function () {
+  describe("Delimited String Addition", function () {
+    it("should return 0 when string is empty", function () {
       expect(stringCalculator.addDelimitedString("")).to.be.equal(0);
     });
-    it("should return same number when string has only one number", function() {
+    it("should return same number when string has only one number", function () {
       expect(stringCalculator.addDelimitedString("1")).to.be.equal(1);
       expect(stringCalculator.addDelimitedString("121")).to.be.equal(121);
     });
-    it("should return correct sum of two numbers", function() {
+    it("should return correct sum of two numbers", function () {
       expect(stringCalculator.addDelimitedString("1,2")).to.be.equal(3);
       expect(stringCalculator.addDelimitedString("121,4")).to.be.equal(125);
     });
-    it("should return correct sum of any ammount of numbers in the string", function() {
+    it("should return correct sum of any ammount of numbers in the string", function () {
       expect(stringCalculator.addDelimitedString("1,2,3,4,5,6,7,8,9,10")).to.be.equal(55);
       expect(stringCalculator.addDelimitedString("121,4,80")).to.be.equal(205);
     });
-    it("should return correct sum only if strings are seperated by ',' or '\\n'", function() {
+    it("should return correct sum only if strings are seperated by ',' or '\\n'", function () {
       expect(stringCalculator.addDelimitedString("1\n2,3")).to.be.equal(6);
       expect(stringCalculator.addDelimitedString("121,4,\n")).to.be.an('error');
+    });
+    it("should reject any string with negative numbers and return an error with the numbers'", function () {
+      expect(stringCalculator.addDelimitedString("1\n2,3,-4,-5"))
+        .to.be.an("error")
+        .to.have.property("message", "Negative numbers are not allowed, the string contains: -4,-5");
+      expect(stringCalculator.addDelimitedString("121,4,\n-4,-11,-100,80"))
+        .to.be.an('error')
+        .to.have.property('message', "Negative numbers are not allowed, the string contains: -4,-11,-100");
     });
   });
 });
